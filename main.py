6100490a8e8c0ee70ea51187d9a78a9c744d7425
fairouzijerdaoun205 <fairouz.ijerdaoun@univ-tlse3.fr>
2025-12-24 12:37:26 +0100

    update

--- main.py
+++ main.py
@@ -8,67 +8,60 @@ from mgd.mgd import Mgd
 from trajectoire import traj
 
 from simulation import UR3Simulator
-from simulation import control 
+from simulation import control
+from simulation import affiche  
 
 def main():
-    # --- A. CONFIGURATION ---
-    # On utilise simplement la classe comme demandé
     urdf_path = "simulation/ur_description/urdf/ur3_robot.urdf"
     
     if not os.path.exists(urdf_path):
-        print(f"Erreur : Fichier introuvable {urdf_path}")
+        print(f"ERREUR : Fichier introuvable {urdf_path}")
         return
 
     print("Démarrage du simulateur...")
     sim = UR3Simulator(urdf_path, dt=0.001)
     
-    # --- B. GÉNÉRATION DE TRAJECTOIRE ---
-    print("Calcul de la trajectoire...")
+    print("Génération de la trajectoire...")
     res = traj(O=(0.320, -0.028, 0.250), R=0.150, V=0.05)
     
     t_arr = res['t']
     q_ref_list = res['q']
     
-    # --- C. INITIALISATION ---
-    print("Placement initial...")
+    print("Positionnement initial...")
     sim.set_joint_angles(q_ref_list[0])
     
-    # --- D. BOUCLE PRINCIPALE ---
-    print(f"Exécution de la trajectoire ({len(t_arr)} points)...")
+    print(f"Exécution du mouvement ({len(t_arr)} pas)...")
     
     X_reel_list = []
     X_ref_list = []
-    
-    
     verificateur = Mgd(cf.data)
 
     for i in range(len(t_arr)):
-
         sim.set_joint_angles(q_ref_list[i])
         
-
         q_reel, _, _ = control.getJointStates(sim.robot, sim.control_joints)
         
-        # 3. VALIDATION (Calcul positions X, Y, Z)
-        # Position réelle
         verificateur.set_angles(q_reel)
         X_reel_list.append(verificateur.get_position())
         
         verificateur.set_angles(q_ref_list[i])
         X_ref_list.append(verificateur.get_position())
 
-    print("Fin. Affichage des résultats...")
+    print("Simulation terminée.")
 
-    # --- E. AFFICHAGE 
+
+    print("Affichage de la loi de mouvement (via affiche.py)...")
+    affiche.afficheloi_mvt(res['s'][0], res['s'][1], res['s'][2], res['t'], res['commutation'])
+
+    print("Affichage de la comparaison 3D...")
     X_reel = np.array(X_reel_list)
     X_ref = np.array(X_ref_list)
     
-    # Tracé 3D
     fig = plt.figure(figsize=(10, 6))
     ax = fig.add_subplot(1, 1, 1, projection='3d')
-    ax.plot(X_ref[:,0], X_ref[:,1], X_ref[:,2], 'b--', label='Consigne')
-    ax.plot(X_reel[:,0], X_reel[:,1], X_reel[:,2], 'r', linewidth=2, label='Réel')
-    ax.set_title("Validation Trajectoire")
+    ax.plot(X_ref[:,0], X_ref[:,1], X_ref[:,2], 'b--', label='Consigne (Théorique)')
+    ax.plot(X_reel[:,0], X_reel[:,1], X_reel[:,2], 'r', linewidth=2, label='Réel (Simulé)')
+    ax.set_title("Validation Trajectoire 3D")
     ax.set_xlabel('X'); ax.set_ylabel('Y'); ax.set_zlabel('Z')
     ax.legend()
     plt.show()
